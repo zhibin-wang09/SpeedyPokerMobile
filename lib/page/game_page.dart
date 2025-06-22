17a36ee55412b2cba53fbe0ba5aa21770d47bbb2
Zhi Bin <105875767+zhibin-wang09@users.noreply.github.com>
2025-06-21 21:05:18 -0400

    Remove unused import

--- lib/page/game_page.dart
+++ lib/page/game_page.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:logger/logger.dart';
 import 'package:provider/provider.dart';
 import 'package:speedy_poker/enums/animated_card_type.dart';
 import 'package:speedy_poker/enums/destination.dart';
