3e699989007b24145cf9b8c80b96332ebfa64182
Zhi Bin <105875767+zhibin-wang09@users.noreply.github.com>
2025-07-14 19:52:46 -0400

    Format and add socket listener for errorneous events

--- lib/page/form_page.dart
+++ lib/page/form_page.dart
@@ -3,6 +3,7 @@ import 'package:provider/provider.dart';
 import 'package:speedy_poker/enums/mode.dart';
 import 'package:speedy_poker/model/socket.dart';
 import 'package:speedy_poker/page/waiting_page.dart';
+import 'package:fluttertoast/fluttertoast.dart';
 
 class SpeedyPokerFormPage extends StatefulWidget {
   const SpeedyPokerFormPage({super.key, required this.mode});
@@ -21,12 +22,54 @@ class _SpeedyPokerFormPageState extends State<SpeedyPokerFormPage> {
       TextEditingController(); // controller to monitor input change in first input field
   final TextEditingController nameController =
       TextEditingController(); // controller for t
+  late SocketService socketService;
+
+  @override
+  void initState() {
+    super.initState();
+  }
+
+  @override
+  void didChangeDependencies() {
+    super.didChangeDependencies();
+
+    socketService = Provider.of<SocketService>(context, listen: false);
+    socketService.socket.on("game:error", _handleGameError);
+    socketService.socket.on("user:joined", _handleUserJoin);
+  }
 
   @override
   void dispose() {
+    socketService.socket.off("game:error", _handleGameError);
+    socketService.socket.on("user:joined", _handleUserJoin);
     roomIdController.dispose();
     nameController.dispose();
     super.dispose();
+  }
+
+  void _handleUserJoin(dynamic json) {
+    String roomId = json as String;
+    Navigator.push(
+      context,
+      MaterialPageRoute(
+        builder: (context) => SpeedyPokerWaitingPage(roomID: int.parse(roomId)),
+      ),
+    );
+  }
+
+  void _handleGameError(dynamic json) {
+    String errMessage = json as String;
+    Fluttertoast.showToast(
+      msg: errMessage,
+      toastLength: Toast.LENGTH_SHORT,
+      gravity: ToastGravity.BOTTOM,
+      timeInSecForIosWeb: 2,
+      backgroundColor: const Color.fromARGB(255, 229, 104, 116),
+      textColor: Colors.white,
+      fontSize: 16.0,
+    );
+
+    Navigator.popUntil(context, (route) => route.settings.name == '/home');
   }
 
   Widget _inputField(
@@ -90,153 +133,121 @@ class _SpeedyPokerFormPageState extends State<SpeedyPokerFormPage> {
                 mainAxisAlignment: MainAxisAlignment.center,
                 crossAxisAlignment: CrossAxisAlignment.center,
                 children: <Widget>[
-                  Consumer<SocketService>(
-                    builder: (context, socketService, child) {
-                      return Column(
-                        children: [
-                          _inputField(
-                            'Player Name',
-                            'Ex. Raisa',
-                            (value) {
-                              if (value == null || value.isEmpty) {
-                                return 'You must have a user name';
-                              }
-                              return null;
-                            },
-                            nameController,
-                            _nameInputKey,
-                          ),
-                          const SizedBox(height: 40),
-                          widget.mode == Mode.joinTargetRoom
-                              ? _inputField(
-                                  'Game Code',
-                                  'Ex. 4242024',
-                                  (value) {
-                                    int? val = int.tryParse(value!);
-                                    if (val == null || value.isEmpty) {
-                                      return 'Please enter room number (Ex. 1910)';
-                                    } else if (val < 0) {
-                                      return 'Room numbers can only be positive';
-                                    }
-                                    return null;
-                                  },
-                                  roomIdController,
-                                  null,
-                                )
-                              : const SizedBox.shrink(),
-                          const SizedBox(height: 10),
-                          widget.mode == Mode.create
-                              ? _button(
-                                  () {
-                                    if (_nameInputKey.currentState!
-                                        .validate()) {
-                                      String playerName = nameController.text;
-
-                                      socketService.emit('user:create', [
-                                        playerName,
-                                      ]);
-
-                                      SocketService().socket.on(
-                                        'user:joined',
-                                        (roomID) {
-                                          Navigator.push(
-                                            context,
-                                            MaterialPageRoute(
-                                              builder: (context) =>
-                                                  SpeedyPokerWaitingPage(
-                                                    roomID: int.parse(roomID),
-                                                  ),
-                                            ),
-                                          );
-                                        },
-                                      );
-                                    }
-                                  },
-                                  Colors.blue,
-                                  'CREATE GAME',
-                                  socketService,
-                                )
-                              : const SizedBox.shrink(),
-                          const SizedBox(height: 10),
-                          widget.mode == Mode.joinTargetRoom
-                              ? _button(
-                                  () {
-                                    if (_formkey.currentState!.validate()) {
-                                      int roomID = int.parse(
-                                        roomIdController.text,
-                                      );
-                                      String playerName = nameController.text;
-
-                                      // join the game room
-                                      socketService.emit('user:join', [
-                                        roomID,
-                                        playerName,
-                                      ]);
-
-                                      SocketService().socket.on(
-                                        'user:joined',
-                                        (roomID) {
-                                          Navigator.push(
-                                            context,
-                                            MaterialPageRoute(
-                                              builder: (context) =>
-                                                  SpeedyPokerWaitingPage(
-                                                    roomID: int.parse(roomID),
-                                                  ),
-                                            ),
-                                          );
-                                        },
-                                      );
-                                    }
-                                  },
-                                  Colors.green,
-                                  'JOIN VIA CODE',
-                                  socketService,
-                                )
-                              : const SizedBox.shrink(),
-                          widget.mode == Mode.joinAnyRoom
-                              ? _button(
-                                  () {
-                                    if (_formkey.currentState!.validate()) {
-                                      String playerName = nameController.text;
-
-                                      // join the game room
-                                      socketService.emit('user:join_any', [
-                                        playerName,
-                                      ]);
-
-                                      SocketService().socket.on(
-                                        'user:joined',
-                                        (roomID) {
-                                          Navigator.push(
-                                            context,
-                                            MaterialPageRoute(
-                                              builder: (context) =>
-                                                  SpeedyPokerWaitingPage(
-                                                    roomID: int.parse(roomID),
-                                                  ),
-                                            ),
-                                          );
-                                        },
-                                      );
-                                    }
-                                  },
-                                  Colors.red,
-                                  'JOIN ANY GAME',
-                                  socketService,
-                                )
-                              : const SizedBox.shrink(),
-                          _button(
-                            () {
-                              Navigator.pop(context);
-                            },
-                            Colors.indigo,
-                            'GO BACK',
-                            socketService,
-                          ),
-                        ],
-                      );
+                  _inputField(
+                    'Player Name',
+                    'Ex. Raisa',
+                    (value) {
+                      if (value == null || value.isEmpty) {
+                        return 'You must have a user name';
+                      }
+                      return null;
                     },
+                    nameController,
+                    _nameInputKey,
+                  ),
+                  const SizedBox(height: 40),
+                  widget.mode == Mode.joinTargetRoom
+                      ? _inputField(
+                          'Game Code',
+                          'Ex. 4242024',
+                          (value) {
+                            int? val = int.tryParse(value!);
+                            if (val == null || value.isEmpty) {
+                              return 'Please enter room number (Ex. 1910)';
+                            } else if (val < 0) {
+                              return 'Room numbers can only be positive';
+                            }
+                            return null;
+                          },
+                          roomIdController,
+                          null,
+                        )
+                      : const SizedBox.shrink(),
+                  const SizedBox(height: 10),
+                  widget.mode == Mode.create
+                      ? _button(
+                          () {
+                            if (_nameInputKey.currentState!.validate()) {
+                              String playerName = nameController.text;
+
+                              socketService.emit('user:create', [playerName]);
+
+                              SocketService().socket.on('user:joined', (
+                                roomID,
+                              ) {
+                                Navigator.push(
+                                  context,
+                                  MaterialPageRoute(
+                                    builder: (context) =>
+                                        SpeedyPokerWaitingPage(
+                                          roomID: int.parse(roomID),
+                                        ),
+                                  ),
+                                );
+                              });
+                            }
+                          },
+                          Colors.blue,
+                          'CREATE GAME',
+                          socketService,
+                        )
+                      : const SizedBox.shrink(),
+                  const SizedBox(height: 10),
+                  widget.mode == Mode.joinTargetRoom
+                      ? _button(
+                          () {
+                            if (_formkey.currentState!.validate()) {
+                              int roomID = int.parse(roomIdController.text);
+                              String playerName = nameController.text;
+
+                              // join the game room
+                              socketService.emit('user:join', [
+                                roomID,
+                                playerName,
+                              ]);
+                            }
+                          },
+                          Colors.green,
+                          'JOIN VIA CODE',
+                          socketService,
+                        )
+                      : const SizedBox.shrink(),
+                  widget.mode == Mode.joinAnyRoom
+                      ? _button(
+                          () {
+                            if (_formkey.currentState!.validate()) {
+                              String playerName = nameController.text;
+
+                              // join the game room
+                              socketService.emit('user:join_any', [playerName]);
+
+                              SocketService().socket.on('user:joined', (
+                                roomID,
+                              ) {
+                                Navigator.push(
+                                  context,
+                                  MaterialPageRoute(
+                                    builder: (context) =>
+                                        SpeedyPokerWaitingPage(
+                                          roomID: int.parse(roomID),
+                                        ),
+                                  ),
+                                );
+                              });
+                            }
+                          },
+                          Colors.red,
+                          'JOIN ANY GAME',
+                          socketService,
+                        )
+                      : const SizedBox.shrink(),
+                  _button(
+                    () {
+                      Navigator.pop(context);
+                    },
+                    Colors.indigo,
+                    'GO BACK',
+                    socketService,
                   ),
                 ],
               ),
